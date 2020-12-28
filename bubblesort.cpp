bca407cb6041ab3c63106927a9e1ee3759e07e42
ishan0jain <59369197+ishan0jain@users.noreply.github.com>
2020-12-28 17:26:01 +0530

    initialization

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,10 +1,7 @@
 #include<iostream>
-#include<algorithm>
 using namespace std;
 
 int main(){
-  int arr[] = {1,2,3,4,5};
-  sort(arr, arr+5);
-  cout<<"Sorted"<<endl;
+  int array[10] ;
   return 0;
 }
